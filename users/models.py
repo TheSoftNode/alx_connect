7aa46c7fa9c78d05d21006c437a1cba5708a1232
TheSoftNode <thesoftnode@gmail.com>
2024-11-06 15:11:07 +0100

    Trying to deploy alx connect

---
+++ users/models.py
@@ -0,0 +1,83 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
+from django.conf import settings
+import random
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field is required")
+        if not extra_fields.get('first_name'):
+            raise ValueError("The First Name field is required")
+        if not extra_fields.get('last_name'):
+            raise ValueError("The Last Name field is required")
+            
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+    first_name = models.CharField(_('first name'), max_length=150, blank=False)  
+    last_name = models.CharField(_('last name'), max_length=150, blank=False)   
+    is_verified = models.BooleanField(default=False)
+    verification_code = models.CharField(max_length=6, null=True, blank=True)
+    reset_password_token = models.CharField(max_length=100, null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['password', 'first_name', 'last_name'] 
+
+    objects = CustomUserManager()
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} <{self.email}>"
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_short_name(self):
+        return self.first_name
+    
+    def send_verification_email(self):
+        code = ''.join(random.choices('0123456789', k=6))
+        self.verification_code = code
+        self.save()
+        
+        send_mail(
+            'Verify Your Email',
+            f'Your verification code is: {code}',
+            settings.DEFAULT_FROM_EMAIL,
+            [self.email],
+            fail_silently=False,
+        )
+
+    def send_reset_password_email(self):
+        token = default_token_generator.make_token(self)
+        self.reset_password_token = token
+        self.save()
+        
+        send_mail(
+            'Reset Your Password',
+            f'Use this token to reset your password: {token}',
+            settings.DEFAULT_FROM_EMAIL,
+            [self.email],
+            fail_silently=False,
+        )
---
+++ users/urls.py
@@ -0,0 +1,14 @@
+from django.urls import re_path
+
+from . import views
+
+urlpatterns = [
+    re_path('signup', views.signup),
+    re_path('login', views.login),
+    re_path('test_token', views.test_token),
+    re_path('verify-email/', views.verify_email, name='verify-email'),
+    re_path('request-password-reset/', views.request_password_reset, name='request-password-reset'),
+    re_path('reset-password/', views.reset_password, name='reset-password'),
+    re_path('change-password/', views.change_password, name='change-password'),
+    re_path('resend-verification/', views.resend_verification, name='resend-verification'),
+]
